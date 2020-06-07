5264332ef0fddb3bb742958018168f92a1869a7d
FedirVolkov <64549890+FedirVolkov@users.noreply.github.com>
2020-06-08 00:08:41 +0300

    Update kurs_nbu.py

--- kurs_nbu.py
+++ kurs_nbu.py
@@ -1,16 +1,11 @@
-'''приклад коду для бат-файла:
-@echo off
-start cmd.exe /d /k "C:\Users\Home\AppData\Local\Programs\Python\Python38-32\python.exe C:\Users\Home\kurs_nbu.py"
-exit /b
-'''
-
 from datetime import date
 import requests
 from decimal import Decimal as dc
-today=date.today().strftime("%d.%m.%Y")
-base=["USD", "EUR", "RUB", "PLN", "CZK", "GBP", "CAD", "CNY"]
-acts = {1: "репорт в файл", 2: "курс валюти", 3: "конвертер в гривню", 4: "конвертер", 5: "репорт на дату"}
-coms = [str(k) for k in acts]
+today = date.today().strftime("%d.%m.%Y")
+base = ["USD", "EUR", "RUB", "PLN", "CZK", "GBP", "CAD", "CNY"]
+acts = {"0": "список команд", "1": "репорт в файл", "2": "курс валюти", "3": "конвертер в гривню", \
+        "4": "конвертер", "5": "репорт на дату", "Enter": "завершення"}
+help = [f"{k} - {v}" for k, v in acts.items()]
 d, c = False, False
 
 def try_func(function):
@@ -32,7 +27,7 @@ def try_func(function):
         return(str(type(exception).__name__)+":", exception)
         
 def get_cur_dic(date = today):
-    url = 'https://bank.gov.ua/ua/markets/exchangerates?date='+date+'&period=daily'
+    url = 'https://bank.gov.ua/ua/markets/exchangerates?date=' + date + '&period=daily'
     try:
         nbu_req = requests.get(url)
         ver = "Verified"
@@ -43,10 +38,10 @@ def get_cur_dic(date = today):
     cur_dic={"UAH":["Українська гривня", dc(1)]}
     for i, line in enumerate(nbu_list):
         if "на дату" in line:
-            day=line.strip()[63:73]+"\n"
+            day = line.strip()[63:73]+"\n"
         if '="Код літерний' in line:
-            list_cur=[nbu_list[i+j].strip() for j in range(7)]
-            cur_dic[list_cur[0][30:33]]=[" ".join(list_cur[4].split()[:-1]), \
+            list_cur = [nbu_list[i+j].strip() for j in range(7)]
+            cur_dic[list_cur[0][30:33]] = [" ".join(list_cur[4].split()[:-1]), \
                                          dc(list_cur[6][32:list_cur[6].find("</td>")].replace(",", ".")) / \
                                          int(list_cur[1][42:list_cur[1].find("</td>")])]
     nbu_req.close()
@@ -62,9 +57,9 @@ def report_in_file(date = today):
     text = ver + "\n" + day
     for i, k in cur_dic.items():
         if i in base:
-            text+="\n"+i+" "+str(k[1])
-    rates=open("rates.txt", "a")
-    rates.write(text+"\n\n")
+            text += "\n" + i + " " + str(k[1])
+    rates = open("rates.txt", "a")
+    rates.write(text + "\n\n")
     rates.close()
     print(ver, day)
     return("Репорт збережено в rates.txt.")
@@ -74,19 +69,19 @@ def get_kurs(date = today):
     cur = input("Валюта, Enter - повний список: ").upper()
     while cur not in cur_dic or cur == "":
         if cur:
-            print("Валюти"+cur+"немає.")
+            print("Валюти" + cur + "немає.")
         else:
             print(*cur_dic)
         cur = input("Валюта, Enter - повний список, exit - вихід: ").upper()
         if cur in "EXIT":
             return "\n"
     else:
-        return(str(cur_dic[cur][1])+" гривень за один "+cur_dic[cur][0]+".")
+        return(str(cur_dic[cur][1]) + " гривень за один " + cur_dic[cur][0] + ".")
         
 def convert_cur(date = today, cur2 = "UAH"):
     ver, day, cur_dic = get_cur_dic(date)
     print("З валюти?")
-    msg_c = ", ".join([str(i)+" - "+el for i, el in enumerate(base)])
+    msg_c = ", ".join([str(i) + " - " + el for i, el in enumerate(base)])
     cn = input("l - повний список валют, Enter - короткий список: ").upper()
     if cn == "l":
         print(*cur_dic)
@@ -100,7 +95,7 @@ def convert_cur(date = today, cur2 = "UAH"):
             cn = "USD"
     cur = (base[int(cn)] if cn.isdigit() else cn)
     if cur not in cur_dic:
-        return "Валюти "+cur+" немає."
+        return "Валюти " + cur + " немає."
     print(cur)
     if cur2 != "UAH":
         print("В яку валюту?")
@@ -112,12 +107,12 @@ def convert_cur(date = today, cur2 = "UAH"):
                 cn2 = "UAH"
         elif cn2 == "":
             print(msg_c)
-            cn2 = input("Валюта:").upper()
+            cn2 = input("Валюта: ").upper()
             if cn2 == "":
                 cn2 = "UAH"
         cur2 = (base[int(cn2)] if cn2.isdigit() else cn2)
     if cur2 not in cur_dic:
-        return "Валюти "+cur+" немає."
+        return "Валюти " + cur + " немає."
     print(cur2)
     while True:
         try:
@@ -128,48 +123,49 @@ def convert_cur(date = today, cur2 = "UAH"):
         except ValueError:
             continue
         break
-    result = float((cur_dic[cur][1]*dc(x))/cur_dic[cur2][1])
+    result = float((cur_dic[cur][1] * dc(x)) / cur_dic[cur2][1])
     print(ver, day)
-    return(str(x)+" "+cur+" = "+str(round(result, 2))+" "+cur2)
+    return(str(x) + " " + cur + " = " + str(round(result, 2)) + " " + cur2)
 
-def response_to_com(num):
+def response_to_com(com):
     global d, c
-    if num not in range(1,6):
-        return None
+    msg_res = "На дату? Формат - дд.мм.рррр. Enter - зараз: "
+    if com == "0":
+        print(*help)
+    elif com == "1":
+        print(try_func(report_in_file))
+    elif com == "2":
+        d = input(msg_res)
+        print(try_func(get_kurs))
+    elif com == "3":
+        d = input(msg_res)
+        print(try_func(convert_cur))
+    elif com == "4":
+        d = input(msg_res)
+        c = True
+        print(try_func(convert_cur))
+    elif com == "5":
+        d = input(msg_res)
+        print(try_func(daily_report))
+    elif com == "Enter":
+        print("Натиснути на Enter, а не ввести це слово!")
     else:
-        msg_res = "На дату? Формат - дд.мм.рррр. Enter - зараз: "
-        if num == 1:
-            print(try_func(report_in_file))
-        elif num == 2:
-            d = input(msg_res)
-            print(try_func(get_kurs))
-        elif num == 3:
-            d = input(msg_res)
-            print(try_func(convert_cur))
-        elif num == 4:
-            d = input(msg_res)
-            c = True
-            print(try_func(convert_cur))
-        elif num == 5:
-            d = input(msg_res)
-            print(try_func(daily_report))
+        print("Не знаю, що робити.")
             
             
 act = input("Enter - курс валют зараз, 0 - список команд: ")
 if act == "":
     daily_report()
-    act = True
-elif act in coms:
-    response_to_com(int(act))
-elif act == "0":
-    print(*(f"{a} - {b}" for a, b in acts.items()))
+elif act in acts:
+    response_to_com(act)
+else:
+    print(*help)
 
-while act:
-    act = input("Щось ще? 0 - список команд, Enter - завершення: ")
-    while act == "0":
-        print(*(f"{a} - {b}" for a, b in acts.items()))
-        act = input("Enter - завершення: ")
-    if act in coms:
-        response_to_com(int(act))
-    else:
+while True:
+    act = input("Щось ще? ")
+    if act in acts:
+        response_to_com(act)
+    elif act == "":
         break
+    else:
+        print("Enter - завершення")
